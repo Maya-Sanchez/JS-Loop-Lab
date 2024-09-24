f079f83de1e97b8f4bea319f97b37874b56c5ea6
Maya-Sanchez <sanchez.y.maya@gmail.com>
2024-09-24 19:24:28 -0400

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS-Loop-Lab
+I did this a little while ago but didn't submit anything. I've attempted to complete it and submit the parts that I completed. 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// Part 1: Fizz Buzz
 // for (let i=1;i<101;i++){
 //  console.log(i);
 //     }
@@ -22,7 +23,7 @@
 //     } else (console.log(i))
 // }
 
- 
+ // Part 2: Prime Time
 
 // take input from the user
 // const number = parseInt(prompt("Enter a positive number: "));
@@ -50,7 +51,7 @@
 //         console.log(`${number} is a not prime number`);
 //     }
 // }
-
+// //Part 3: Feeling Loopy
 // let csv =`ID,Name,Occupation,Age\n42,Bruce,Knight,41\n
 // 57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n
 // 98,Bill,Doctor’s Assistant,26`
@@ -62,7 +63,7 @@
 //     console.log(character);
 //     let rows = csv.split(`/n`);
 //     let header = rows.shift().split(`,`);
-    
+// } 
 
 // const rows = csv.split("\n");
 // for(let row of rows) {
@@ -73,54 +74,24 @@
 // }
 
 
+//Part 4: Transforming Data
 
-
-
-// //    let rows = csv.split(`\n`)
-// //    let cell = rows[i].split(',');
+//    let rows = csv.split(`\n`)
+//    let cell = rows[i].split(',');
 
 //    console.log(cell)
     
     
-
-
-    
-
-
-
-    
-    // let result = " ";
-    // for (let i=1; i<=7; i++) {
-    // result += "#";
-    // console.log(result);
-
-    // }
- 
-
-
-
-
-
-
-
-
-
-
-
-
+//     let result = " ";
+//     for (let i=1; i<=7; i++) {
+//     result += "#";
+//     console.log(result);*-
+//         }
 
 // const csvArray = csv.split(`\n`)
 // console.log(csvArray)
 
 
-
-
-
-
-
-
-
-
 // const rows = csv.split("\n");
 // for(let row of rows) {
 //     let cell1, cell2, cell3, cell4;
@@ -129,34 +100,34 @@
 //     console.log(cell1, cell2, cell3, cell4);
 // }
 
-// const csv =
-//   "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
-// const rows = csv.split("\n");
-// // console.log(rows);
-// for (let row of rows) {
-//   let cell1, cell2, cell3, cell4;
-//   const cells = row.split(",");
-//   // console.log(cells);
-//   let counter = 1;
-//   for (let cell of cells) {
-//     switch (counter) {
-//       case 1:
-//         cell1 = cell;
-//         break;
-//       case 2:
-//         cell2 = cell;
-//         break;
-//       case 3:
-//         cell3 = cell;
-//         break;
-//       case 4:
-//         cell4 = cell;
-//         break;
-//       default:
-//         break;
-//     }
-//     counter++;
-//   }
-//   console.log(cell1, cell2, cell3, cell4);
-// }
+const csv =
+  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+const rows = csv.split("\n");
+// console.log(rows);
+for (let row of rows) {
+  let cell1, cell2, cell3, cell4;
+  const cells = row.split(",");
+  // console.log(cells);
+  let counter = 1;
+  for (let cell of cells) {
+    switch (counter) {
+      case 1:
+        cell1 = cell;
+        break;
+      case 2:
+        cell2 = cell;
+        break;
+      case 3:
+        cell3 = cell;
+        break;
+      case 4:
+        cell4 = cell;
+        break;
+      default:
+        break;
+    }
+    counter++;
+  }
+  console.log(cell1, cell2, cell3, cell4);
+}
 
